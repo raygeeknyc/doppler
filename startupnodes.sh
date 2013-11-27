cf4d3399a3a7fed5ec34b2baacc25b512e2ee5ae
raymond <raymond@b44fb299-85a0-462d-bbcc-71649ff9a538>
2013-11-27 22:09:18 +0000

    Fixed startup script internal path

--- startupnodes.sh
+++ startupnodes.sh
@@ -3,5 +3,5 @@ SUBNET="192.168.1"
 for node in 101 102 103 104 105 106 107 108
 do
 	echo ${SUBNET}.${node}
-	ssh root@${SUBNET}.${node} "cd doppler;nohup run_renderer.sh &"
+	ssh root@${SUBNET}.${node} "cd doppler;./run_renderer.sh &"
 done
