9ad480821d46e0b2ba89c060eb2343034d5f820d
raymond <raymond@b44fb299-85a0-462d-bbcc-71649ff9a538>
2013-12-11 12:38:06 +0000

    Expanded depth test to show total shifts and an ASCII art map of aggregated shifts per 10x10 pixel group. Untested.
    I'll use this to confirm the existence of sensor noise and if there is, to test the sparseness of noise to see if I can eliminate it by considering aggregate changes per cell and ignoring low counts.

--- test_depth.py
+++ test_depth.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 # arg = kinect device index, from 0
+# arg = threshold above which we consider a sensor spots depth to be changed
 import copy
-#import freenect
+import freenect
 import numpy as np
 import sys
 
-THRESHOLD = 50
-
-if len(sys.argv) != 2:
-	print >> sys.stderr, "usage %s kinect-device-index" % sys.argv[0]
+if len(sys.argv) != 3:
+	print >> sys.stderr, "usage %s kinect-device-index threshold" % sys.argv[0]
 	sys.exit(255)
 
-old_depth, timestamp = freenect.sync_get_depth()
-print 'Kinect device %d, Depth[][] %d,%d\n' % (sys.argv[1], len(old_depth) ,len(old_depth[0]))
+CELL_SCALING_FACTOR = 10
+threshold = sys.argv[2]
+old_depth, timestamp = freenect.sync_get_depth(sys.argv[1]])
+height = len(old_depth)
+width = len(old_depth[0])
+print 'Kinect device %d, Depth map %d X %d\n' % (sys.argv[1], height, width)
+cell_map = [[0]*(width/CELL_SCALING_FACTOR)]*(height/CELL_SCALING_FACTOR)
+print 'Cell map %d X %d\n' % (len(cell_map), len(cell_map[0]))
 min_depth = None
 max_depth = None
 while 1:
@@ -24,9 +29,20 @@ while 1:
     for row in range(0, len(depth)):
       delta.append([])
       for col in range(0, len(depth[row])):
-        # print "%d,%d : %d -> %d = %d\n" % (row, col, int(old_depth[row][col]), int(depth[row][col]), int(old_depth[row][col]) - int(depth[row][col]))
         delta[row].append(int(old_depth[row][col]) - int(depth[row][col]))
-        if abs(delta[row][col]) > THRESHOLD:
+        if abs(delta[row][col]) >= threshold:
+	  cell_map[int(row/CELL_SCALING_FACTOR)][int(col/CELL_SCALING_FACTOR)] += 1
           shifts += 1
-    print "%d shifts\n" % shifts
+    print "%d shifts >= %d" % (shifts, threshold)
+    print "min:max depth %d : %d"\n" % (min_depth, max_depth)
+    print "\nMap of %d X %d pixel aggregate shifts\n\n"
+    for cell_row in range(0, len(cell_map):
+      for cell_col in range(0, len(cell_map[0])):
+        if cell_map[cell_row][cell_col] == 0:
+          print ' '
+        elif cell_map[cell_row][cell_col] > 9:
+          print '+'
+        else
+          print cell_map[cell_row][cell_col]
+      print '\n'
     old_depth = copy.deepcopy(depth)
