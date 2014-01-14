858cae0a6bdcac41a7f77ea283744d5c3a4d7e7a
raymond <raymond@b44fb299-85a0-462d-bbcc-71649ff9a538>
2014-01-14 02:56:53 +0000

    Updated speed test to show the number of accesses in a typical refresh cycle among all renderers.

--- testdicts.py
+++ testdicts.py
@@ -1,29 +1,32 @@
 import time
 
+COLS=1900
+ROWS=150
+
 l = []
-for x in range(300):
+for x in range(COLS):
         r = []
-        for y in range(100):
+        for y in range(ROWS):
                 r.append(x+y)
         l.append(r)
 
 d = {}
-for x in range(300):
+for x in range(COLS):
         r = {}
-        for y in range(100):
+        for y in range(ROWS):
                 r[y] = (x+y)
         d[x] = r
 
 print "list[%d][%d]" % (len(l), len(l[0]))
 for iter in range(5):  
         start = time.time()
-        for x in range(300):
-                for y in range(100):
+        for x in range(COLS):
+                for y in range(ROWS):
                         a = l[x][y]
         print "list took %f" % (time.time() - start)
 
         start = time.time()
-        for x in range(300):
-                for y in range(100):
+        for x in range(COLS):
+                for y in range(ROWS):
                         a = d[x][y]
         print "dict took %f" % (time.time() - start)
