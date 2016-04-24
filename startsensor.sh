9e78add8d281c044febce9a4489fc8d00ed743f2
Raymond Blum <raymond@Raymonds-MacBook-Air.local>
2016-04-24 09:41:59 -0400

    Cleaned up startup and deployment scripts

--- startsensor.sh
+++ startsensor.sh
@@ -2,5 +2,8 @@
 sudo rmmod gspca_kinect
 sudo modprobe gspca_kinect
 sudo python set_tilt.py -22
-#sudo python multisensor.py
-sudo python singlesensor.py
+if [[ "${1}" -eq "single" ]];then
+  sudo python singlesensor.py
+else
+  sudo python multisensor.py
+fi
--- startupnodes.sh
+++ startupnodes.sh
@@ -3,5 +3,5 @@ SUBNET="192.168.1"
 for node in 101 105 102 106 103 107 104 108
 do
 	echo ${SUBNET}.${node}
-	ssh root@${SUBNET}.${node} "cd doppler;./run_renderer.sh &"
+	ssh root@${SUBNET}.${node} "cd doppler;killall -9 run_renderer.sh;killall -9 python;./run_renderer.sh &"
 done
